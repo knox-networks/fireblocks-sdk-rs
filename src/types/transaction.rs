41488eed53043ea91e31984fc8c45b85c410c2e4
Jason Klein <jklein@knox-networks.com>
2025-06-30 11:18:22 -0500

    Add extra parameters (#2)
    This is required to support custom contract call for to call Blockdaemon
    staking contract

--- src/types/transaction.rs
+++ src/types/transaction.rs
@@ -364,7 +364,8 @@ pub struct TransactionArguments {
   #[serde(rename = "customerRefId", skip_serializing_if = "Option::is_none")]
   pub customer_ref_id: Option<String>,
 
-  // pub extra_parameters: Option<ExtraParameters>,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub extra_parameters: Option<ExtraParameters>,
   // pub extra_parameters: Option<String>,
   #[serde(rename = "treatAsGrossAmount", skip_serializing_if = "Option::is_none")]
   pub treat_as_gross_amount: Option<bool>,
